915916b028a22648874998d629499aa1d9106d94
Mari Miyachi <mari.a.miyachi@gmail.com>
2012-09-07 05:17:50 -0700

    Working on visit

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -9,26 +9,16 @@ class SitesController < ApplicationController
   # GET /sites/1.json
   def show
     @site = Site.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @site }
-    end
   end
 
   # GET /sites/new
   # GET /sites/new.json
   def new
     @site = Site.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @site }
-    end
   end
 
-  # GET /sites/1/edit
-  def edit
+  # GET /sites/1/visit
+  def visit
     @site = Site.find(params[:id])
   end
 
@@ -36,32 +26,12 @@ class SitesController < ApplicationController
   # POST /sites.json
   def create
     @site = Site.new(params[:site])
-
-    respond_to do |format|
-      if @site.save
-        format.html { redirect_to @site, notice: 'Site was successfully created.' }
-        format.json { render json: @site, status: :created, location: @site }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @site.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PUT /sites/1
   # PUT /sites/1.json
   def update
     @site = Site.find(params[:id])
-
-    respond_to do |format|
-      if @site.update_attributes(params[:site])
-        format.html { redirect_to @site, notice: 'Site was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @site.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /sites/1
@@ -69,10 +39,5 @@ class SitesController < ApplicationController
   def destroy
     @site = Site.find(params[:id])
     @site.destroy
-
-    respond_to do |format|
-      format.html { redirect_to sites_url }
-      format.json { head :no_content }
-    end
   end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,8 @@
+# == DB Information
+# Table name: sites
+# id    :integer    not null, primary key
+# count :integer
+
 class Site < ActiveRecord::Base
   attr_accessible :count
 end
