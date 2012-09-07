c7a06a72000cced0e97a1465343297430e57bd72
Mari Miyachi <mari.a.miyachi@gmail.com>
2012-09-07 13:55:33 -0700

    delete lines in controller

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -6,18 +6,6 @@ class SitesController < ApplicationController
     @sites = Site.all
   end
 
-  # GET /sites/1
-  # GET /sites/1.json
-  def show
-    @site = Site.find(params[:id])
-  end
-
-  # GET /sites/new
-  # GET /sites/new.json
-  def new
-    @site = Site.new
-  end
-
   # GET /sites/1/visit
   # Register visit to [id number] site
   def visit
@@ -26,22 +14,4 @@ class SitesController < ApplicationController
     @site.update_attributes(count: @viewcount)
   end
 
-  # POST /sites
-  # POST /sites.json
-  def create
-    @site = Site.new(params[:site])
-  end
-
-  # PUT /sites/1
-  # PUT /sites/1.json
-  def update
-    @site = Site.find(params[:id])
-  end
-
-  # DELETE /sites/1
-  # DELETE /sites/1.json
-  def destroy
-    @site = Site.find(params[:id])
-    @site.destroy
-  end
 end
