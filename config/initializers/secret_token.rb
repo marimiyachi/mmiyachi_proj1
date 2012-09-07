0c2d677694b90d240c18845673968b7057f1c8de
Mari Miyachi <mari.a.miyachi@gmail.com>
2012-09-06 18:03:12 -0700

    created empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MmiyachiProj1::Application.config.secret_token = '480041dd219e03bb2ce837ed1a5937dee18e2c34eb1329586e25e7a49bb4e95527f0a30b9194bf4cadf6a1aec73a84b18266e9598f8742086347054f20c804cd'
