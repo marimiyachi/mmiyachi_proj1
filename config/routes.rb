559103d568f4456787f696e55d5a689852c7718f
Mari Miyachi <mari.a.miyachi@gmail.com>
2012-09-19 21:05:12 -0700

    css changes and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Proj1::Application.routes.draw do
+  root to: 'static_pages#home'
+  
   get "static_pages/home"
   get "static_pages/about"
   get "static_pages/help"
+
+  match '/home', to: 'static_pages#home'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
 
   resources :pages
   resources :users
